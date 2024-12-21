3ca10467202ab2006b9f99b4e21cd8fabdf9fa32
Kommandor <a.tsymbaliuk2007@gmail.com>
2024-12-21 13:48:45 +0200

    add task 2 prac

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,30 +8,41 @@ namespace ConsoleApp4
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static  bool FibonacciEquals(int number)
         {
 
+            if (number < 0)
+                return false;
+
+            int a = 0;
+            int b = 1;
+
+            while (a <= number)
+            {
+                if (a == number)
+                    return true;
+
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+
+            return false;
+        }
+
+        static void Main()
+        {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-
-            Console.Write("Enter first num of range: ");
+            Console.Write("Enter num: ");
             var num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter first num of range: ");
+            Console.Write("Enter range: ");
             var num2 = Convert.ToInt32(Console.ReadLine());
-            long dobutok = 1;
 
-            if (num2 < num1)
-            {
-                (num1, num2) = (num2, num1);
-            }
+            bool isFibonacci = FibonacciEquals(num1);
 
-            for (int i = num1; i <= num2; i++)
-            {
-                dobutok *= i;
-            }
-
-            Console.WriteLine($"Добуток чисел від {num1} до {num2} дорівнює: {dobutok}");
+            Console.WriteLine(isFibonacci);
         }
     }
 }
